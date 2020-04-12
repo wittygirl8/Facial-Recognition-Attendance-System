89de8df74f72fe8eddb9147e26afc4936e7f5af2
Riddhi <riddhisinghreena238@gmail.com>
2020-04-12 18:34:25 +0530

    second commit

--- faces.py
+++ faces.py
@@ -1,13 +1,52 @@
 import numpy as np
 import cv2
+import pickle
 
 
+face_cascade = cv2.CascadeClassifier('Resources/haarcascade_frontalface_default.xml')
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {"person's name": 1}
+with open("labels.pickle",'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
 
 cap=cv2.VideoCapture(0)
 
 while True:
     #capture frame by frame
     ret, frame= cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h)in faces:
+        #print(x,y,w,h)
+        roi_gray=gray[y:y+h, x:x+w]
+        roi_color = gray[y :y + h, x :x + w]
+
+        #recognize:
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45 and conf <= 85:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_COMPLEX
+            name = labels[id_]
+            color = (255,255,255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+        img_item="myimg.png"
+        cv2.imwrite(img_item, roi_gray)
+
+    #face frame
+        color = (255,0,0) #BGR 0-255
+        stroke =2
+        end_cord_x= x+w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x,y), (end_cord_x,end_cord_y),color, stroke)
+        
+
 
     #display the resulting frame
     cv2.imshow('frame',frame)
